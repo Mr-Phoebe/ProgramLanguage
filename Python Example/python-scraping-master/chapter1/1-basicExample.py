9e3f20dc63751ed6f1c78a40cad121db17bea561
HaonanWu <whn289467822@outlook.com>
2016-12-20 16:36:39 +0800

    python 2&3

--- Python Example/python-scraping-master/chapter1/1-basicExample.py
+++ Python Example/python-scraping-master/chapter1/1-basicExample.py
@@ -1,4 +1,10 @@
-from urllib.request import urlopen
+import sys
+
+if sys.version_info[0] == 2:
+    from urllib2 import urlopen  # Python 2
+else:
+    from urllib.request import urlopen  # Python3
+
 #Retrieve HTML string from the URL
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 print(html.read())
--- Python Example/python-scraping-master/chapter1/2-beautifulSoup.py
+++ Python Example/python-scraping-master/chapter1/2-beautifulSoup.py
@@ -1,6 +1,12 @@
-from urllib.request import urlopen
+import sys
 from bs4 import BeautifulSoup
+if sys.version_info[0] == 2:
+    from urllib2 import urlopen  # Python 2
+else:
+    from urllib.request import urlopen  # Python3
+
 
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+print bsObj.h1
+print bsObj.prettify()
