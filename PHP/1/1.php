1edaf4279eedab6ba5fa05d1241e91c31f01ae22
HaonanWu <whn289467822@outlook.com>
2017-09-09 00:02:27 -0400

    php

---
+++ PHP/1/1.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>经典循环例子</TITLE>
+</HEAD>
+<BODY>
+<?
+	for($counter = 1; $counter <= 6; $counter++)		//循环6次
+	{
+		print("<B>counter is $counter</B><BR>\n");	//打印6次
+	}	
+?>
+</BODY>
+</HTML>
