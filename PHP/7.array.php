e73a8291fc31f84c5a1b5293923ebd4b5f13d2b3
HaonanWu <whn289467822@outlook.com>
2017-12-06 11:22:54 -0500

    fix bugs

---
+++ PHP/7.array.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<TITLE>สýื้</TITLE>
+</HEAD>
+<BODY>
+<FONT SIZE=5>
+<?
+	$arr = array(
+		'name' => 'user1',
+		'age' => '20',
+		'set' => 'female');
+	$arr2 = array('user1', '20', 'female');
+?>
+</FONT>
+</BODY>
+</HTML> 
---
+++ PHP/8.class.php
@@ -0,0 +1,26 @@
+<HTML>
+<HEAD>
+<TITLE>¿‡</TITLE>
+</HEAD>
+<BODY>
+<FONT SIZE=5>
+<?
+class Person {
+    public $name = 'user1';
+    public $age = '20';
+    public $sex = 'male';
+
+    public function say()
+    {
+        echo "my name is {$this->name}";
+    }
+}
+
+$xi = new Person();
+
+echo $jin->age;
+echo $jin->say();
+?>
+</FONT>
+</BODY>
+</HTML> 
