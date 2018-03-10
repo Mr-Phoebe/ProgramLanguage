df155e24ce2996e4adf0d143893e941ec512306d
Haonan Wu <whn289467822@outlook.com>
2018-03-10 10:09:03 -0500

    category

---
+++ JavaScript/NodeJS/03.third party/n-3-17-arrow-functions/playground/arrow-function.js
@@ -0,0 +1,16 @@
+var square = x => x * x;
+console.log(square(9));
+
+var user = {
+  name: 'Andrew',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  }
+};
+
+user.sayHi(1, 2, 3);
---
+++ JavaScript/NodeJS/03.third party/n-3-9-adding-note/playground/json.js
@@ -0,0 +1,25 @@
+// var obj = {
+//   name: 'Andrew'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Andrew","age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
