7692c25d8e0183ace87974ccf0a75cfadd231c94
Mr-Phoebe <whn289467822@outlook.com>
2018-03-13 22:52:22 -0400

    nodejs express

---
+++ JavaScript/NodeJS/05.deploy/n-5-4-rendering-templates/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
---
+++ JavaScript/NodeJS/05.deploy/n-5-5-advanced-templates/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
