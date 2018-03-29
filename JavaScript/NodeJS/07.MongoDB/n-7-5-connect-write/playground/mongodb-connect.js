5da76245b9ecf06bef6abdf93230bc848fa132d0
Mr-Phoebe <whn289467822@outlook.com>
2018-03-29 17:39:49 -0400

    nodejs mongo

---
+++ JavaScript/NodeJS/07.MongoDB/n-7-5-connect-write/playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne({
+    name: 'Andrew',
+    age: 25,
+    location: 'Philadelphia'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(result.ops);
+  });
+
+  db.close();
+});
