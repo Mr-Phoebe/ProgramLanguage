f7cb01e4b343606ccef63767374c7c3237a17bca
Mr-Phoebe <whn289467822@outlook.com>
2018-03-31 22:05:58 -0400

    nodejs mongoose

---
+++ JavaScript/NodeJS/07.MongoDB/n-7-13-schemas/server/server.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var user = new User({
+  email: 'andrew@example.com   '
+});
+
+user.save().then((doc) => {
+  console.log('User saved', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
+});
